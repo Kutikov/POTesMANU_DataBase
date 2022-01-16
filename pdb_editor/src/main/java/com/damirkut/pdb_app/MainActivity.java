37bad77d40d4b77828f0da7694492704032e8928
Damir A. Kutikov <damirkut@gmail.com>
2022-01-16 10:41:43 +0200

    module refactor

--- pdb_editor/src/main/java/com/damirkut/pdb_app/MainActivity.java
+++ pdb_editor/src/main/java/com/damirkut/pdb_app/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.damirkut.pdb_lib.Core;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("test", Core.getStrByKey("{\"key\":22}", "key"));
     }
 }
